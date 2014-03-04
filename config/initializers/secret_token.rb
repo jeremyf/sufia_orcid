e1640288117f11889edec500acdd961430c82872
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-04 15:45:30 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SufiaOrcid::Application.config.secret_key_base = '7ae0a5b0c12f08a746b3c984dc97b4badef4ec1c5ac930d431a460ed15bc1bb1fd637a60543ca07a9beaca841dcbfedb8ca6fa148b7fae7019dc9c6192620b24'
